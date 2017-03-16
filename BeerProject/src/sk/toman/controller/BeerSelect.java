56d496b19b5d5aab410ba5eb6c98b9c906c1e4d7
tomas.toman <tomas.toman@SKPC012.DWC.local>
2017-03-16 15:00:05 +0100

    beer

---
+++ BeerProject/src/sk/toman/controller/BeerSelect.java
@@ -0,0 +1,25 @@
+package sk.toman.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BeerSelect extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Beer Selection Advice<br>");
+		String c = request.getParameter("color");
+		out.println("<br>Got beer color " + c);
+	}
+}
